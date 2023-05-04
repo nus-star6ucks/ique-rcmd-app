fffae3f41c02e16ee85d18dbaa1ea09d1eb3227f
pcdotfan <i@know.cx>
2023-05-04 18:59:31 +0800

    typo

--- src/pages/Store/Store.vue
+++ src/pages/Store/Store.vue
@@ -107,7 +107,7 @@ export default {
   },
   methods: {
     async onCreateRecStores () {
-      await axios.post(`${process.env.VERCEL_INSTANCE_URL}/admin/producer/api`, {
+      await axios.post(`https://ique-app.vercel.app/admin/producer/api`, {
         userId: this.userId,
         longitude: this.longitude,
         latitude: this.latitude
